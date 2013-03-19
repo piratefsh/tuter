24ff3673063e2f8f0b5c31ca0439f5254e90c2d5
Josh Serbus <JoshSerbus09@gmail.com>
2013-03-19 11:27:24 -0500

    added current_user helper method to home_controller since it is the root and needs the helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   #before_filter :authenticate_user!
-  helper_method :current_user
 
 
   def index
-  end	
-
-  private
-
-  def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end  
+  end	 
 end
