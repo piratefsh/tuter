313013d51bef3df211d1611ead9c73ab61ce521a
Sher Minn Chong <schong7@wisc.edu>
2013-03-20 22:27:26 -0500

    Add rates spinner

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,18 @@
 class SearchController < ApplicationController
   def index
     @users = User.all
-    
+    @rates = Array.new
+
+    start_rate  = 0
+    end_rate    = 100
+    interval    = 10
+    i = start_rate
+
+    until end_rate < i
+        @rates.push("$#{i} - $#{i + interval}")
+        i += interval
+    end
+
     respond_to do |format|
         format.html #search/index/html.erb
 
