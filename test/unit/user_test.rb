6435491421937bd6d5c9793aa5fea0ef56bd2cef
FaizLurman <lurman@wisc.edu>
2013-04-03 11:36:10 -0500

    Unit test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- 	test "user creation"
+ 	test "user_creation must fail" do
  		user = User.new
- 		assert user
- 	end
-
- 	test "destroy user"
- 		user = User.new
- 		user.destroy
- 		assert user = nil
- 	end
+ 		assert user.valid?
+ 	end 	
 end
