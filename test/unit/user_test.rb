e434c915d91f124c19e54e0d3d82fb6150e6a1fd
FaizLurman <lurman@wisc.edu>
2013-04-03 15:41:42 -0500

    Phew, too many commit

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- 	test "user_creation must fail" do
- 		user = User.create
- 		assert !user.valid?
+ 	test "user_creation" do
+ 		user = User.new
+ 		assert !user.valid?, 'Must have first_name, last_name, email, password'
+ 		user.first_name = 'AnotherFaiz'
+ 		user.last_name  = 'AnotherLurman'
+ 		user.email      = 'Anotherfaiz@tuter.me'
+        user.password   = 'PASSWORD' 		
+        assert user.valid?, 'Should pass now'
  	end 	
 end
