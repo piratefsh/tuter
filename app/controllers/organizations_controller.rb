53f7cb0e48750eb2985b7e3ec0425248e534b8a0
FaizLurman <lurman@wisc.edu>
2013-04-10 13:37:12 -0500

    :heart: Try to merge

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,38 @@
+class OrganizationsController < ApplicationController
+  def index
+    @groups = Organization.all
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def new
+    @group = Group.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create 
+    @group = Group.new(params[:group])
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to groups_path }
+      end 
+    end
+  end 
+
+  def destroy
+  end
+
+  def edit
+    # @group = Group.find(params[:group_id])
+
+    respond_to do |format|
+      format.html
+    end
+  end
+end
