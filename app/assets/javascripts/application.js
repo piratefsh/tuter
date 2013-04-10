12a22f27c33c1a5729b4d5e962a1b3cd54611906
Sher Minn Chong <schong7@wisc.edu>
2013-04-09 22:30:30 -0500

    Implement tabs on dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-1.9.0.js
+//= require jquery
+//= require jquery-ui
 //= require_tree .
 
 
---
+++ app/assets/javascripts/groups.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+    $('#create_new').click(function()
+    {
+        $("div#new_group").show();
+        console.log("click") 
+    });
+});
