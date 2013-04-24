d4f93317884644fe1ceb6a1fba0ace59a5156ea8
Sher Minn Chong <schong7@wisc.edu>
2013-04-24 13:03:24 -0500

    Display - for not found tutors or students in groups tab

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 
   def index
     @rates = Array.new
-    @tutors = User.all.role? :tutor
+    # @tutors = User.all.role? :tutor
     @users = User.all
 
     initRates(@rates)
