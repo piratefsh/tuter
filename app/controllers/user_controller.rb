5e87483be48b3451b5bb131d552f35ee46576b20
Sher Minn Chong <schong7@wisc.edu>
2013-05-03 17:54:47 -0500

    Implement only listing user's reviews on user profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
     @review = Review.new
-    @reviews= Review.all
+    @reviews= Review.where("user_id =?", @user.id)
 
     respond_to do |format|
         format.html
