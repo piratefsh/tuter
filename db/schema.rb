c1d300efe0fb3d9215be00527b9773e48681dc7f
Adam Thorson <adamdanielthorson@gmail.com>
2013-05-05 11:25:04 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130503212445) do
+ActiveRecord::Schema.define(:version => 20130503223208) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
