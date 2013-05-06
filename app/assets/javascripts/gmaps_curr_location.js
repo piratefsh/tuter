3d2b483e70b2d46e3fb38193dcdbd68a778fbe66
Sher Minn Chong <schong7@wisc.edu>
2013-05-06 00:05:27 -0500

    Add marker for current user's location

---
+++ app/assets/javascripts/gmaps_curr_location.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+    setInterval(function() {
+        Gmaps.map.createMarker({
+           Lat: Gmaps.map.userLocation.lat(),
+           Lng: Gmaps.map.userLocation.lng(), 
+           rich_marker: null, 
+           title: "My Location",
+           marker_picture: "http://maps.google.com/mapfiles/arrow.png",
+           marker_width: 80,
+           marker_height: 80,
+           marker_anchor: null,
+           shadow_anchor: null,
+           shadow_picture: null,
+           shadow_width: null,
+           shadow_height: null
+       })}, 1000);
+  });
